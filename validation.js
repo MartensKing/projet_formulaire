26753107e50184027d8f3858636901206e0fb42e
adam_M <mamanedam11@gmail.com>
2026-06-28 22:21:48 +0000

    finalisation

--- validation.js
+++ validation.js
@@ -1,16 +1,22 @@
+//travail Adam
+//creation de la fonction qui verifie le mot de passe
 function validerPassword(valeur) {
+    //si la valeur est vide retour faux
     if (!valeur) {
         return { valide: false, message: "le mot de passe est requis" }
-
     }
-    if (valeur.lenght <= 9) {
-        return { valide: false, message: "le mot de passe doit contenir au moins 9 caracteres" }
+    //condition qui oblige a avoir au moins 7 caracteres dans le mot de passe
+    if (valeur.length <= 7) {
+        return { valide: false, message: "le mot de passe ne contient pas assez de caracteres" }
     }
+    //\d signifie un chiffre et !/ inverse le resultat
     if (!/\d/.test(valeur)) {
         return { valide: false, message: "le mot de passe doit contenir au moins un chiffre" }
     }
+    //[A-Z] designe une lettre majuscule et !/ inverse le resultat
     if (!/[A-Z]/.test(valeur)) {
-        return { valide: false, message: "le mot de passe doit contenir au moins une lette majuscule" }
+        return { valide: false, message: "le mot de passe doit contenir au moins une lettre majuscule" }
     }
+    //si toutes les conditions sont remplies le mot de passe est valide
     return { valide: true, message: "Mot de passe valide" }
 }
